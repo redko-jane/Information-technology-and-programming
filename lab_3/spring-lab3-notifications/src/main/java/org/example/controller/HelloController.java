7b1f3f50225455f9e9a726c77410667c02efc8ca
Evgeniya <redko.jenny@gmail.com>
2026-06-10 21:58:49 +0300

    Add files via upload

---
+++ lab_3/spring-lab3-notifications/src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,29 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Привет, Spring Boot!";
+    }
+
+    @GetMapping("/goodbye")
+    public String sayGoodbye() {
+        return "До свидания, Spring Boot!";
+    }
+
+    @GetMapping("/greet")
+    public String sayName(@RequestParam String name) {
+        return "Привет, " + name + "!";
+    }
+
+     @GetMapping("/info")
+    public String userInfo(@RequestParam String name, @RequestParam int age) {
+        return "Пользователь: " + name + ", возраст: " + age;
+    }
+}
---
+++ lab_4/spring-lab3-notifications/src/main/java/org/example/model/enums/NotificationChannel.java
@@ -0,0 +1,8 @@
+package org.example.model.enums;
+
+public enum NotificationChannel {
+    EMAIL,
+    SMS,
+    PUSH,
+    TELEGRAM
+}
